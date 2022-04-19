be8dbba1f2216f00b0f6a79ff80866b02bd35ef0
Daniel Miolan <miolan96@gmail.com>
2022-04-19 19:02:37 -0400

    Add logout feature.

--- scripts/main.js
+++ scripts/main.js
@@ -10,7 +10,22 @@ function toggleMenu() {
 }
 
 const storageLoged = localStorage.getItem('isUserLoged');
-const isUserLoged = storageLoged !== null && storageLoged;
+const isUserLoged = storageLoged !== null && storageLoged == "true";
 const userData =  JSON.parse(localStorage.getItem("user"));
+const logoutD = document.getElementById("desktop-logout");
+const logoutM = document.getElementById("mobile-logout");
 
 document.querySelector("body").className = isUserLoged ? "login-view" : "unloged-view";
+
+logoutD.addEventListener("click", logout);
+logoutM.addEventListener("click", logout);
+
+function logout() { 
+    localStorage.setItem('isUserLoged', false);
+
+    if(window.location.href.indexOf('/pages') >= 0) {
+        window.open(window.location.href.slice(0,window.location.href.indexOf('/pages')) + "/index.html", "_self");
+    } else {
+        window.open("index.html", "_self");
+    }
+}
